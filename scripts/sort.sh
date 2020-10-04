ed492849b23dfec805e78c7a9249faf9d2953ecb
Daniel Hoherd <daniel.hoherd@gmail.com>
2020-10-04 16:01:07 -0700

    Deprecate sort-gitignore.

--- scripts/sort.sh
+++ scripts/sort.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 export LC_ALL=C
 
-for FILE in "${@}" ; do
-  sort -u -o "${FILE}" "${FILE}"
-done
+cat <<EOF 1>&2
+DEPRECATION WARNING: sort-gitignore is deprecated. Use:
+
+  - repo: https://github.com/pre-commit/pre-commit-hooks
+    rev: v3.2.0
+    hooks:
+      - id: file-contents-sorter
+        files: .gitignore
+EOF
+
+exit 1
