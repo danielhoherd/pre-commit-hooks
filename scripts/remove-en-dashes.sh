adaa5f76387c02c41c346a183c1ca292ced9512a
Daniel Hoherd <daniel.hoherd@gmail.com>
2023-09-15 12:24:46 -0400

    Rename two hooks. Add replace-greek-question-mark.

---
+++ scripts/remove-en-dashes.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+export LC_ALL=C
+
+echo "DEPRECATION WARNING: remove-en-dashes is deprecated. Use replace-en-dashes"
+
+exit 1
---
+++ scripts/remove-unicode-non-breaking-spaces.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+export LC_ALL=C
+
+echo "DEPRECATION WARNING: remove-unicode-non-breaking-spaces is deprecated. Use replace-unicode-non-breaking-spaces"
+
+exit 1
